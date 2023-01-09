bd6127f4bb97e3a1d0e23a73d4dcf2bb2e274ff3
Chris Kipp <ckipp@pm.me>
2023-01-09 11:08:45 +0100

    chore(deps): update various deps and scala-cli launcher (#170)

--- src/GenerateIndex.scala
+++ src/GenerateIndex.scala
@@ -1,7 +1,7 @@
 //> using scala "2.13"
-//> using lib "com.softwaremill.sttp.client3::core:3.3.15"
-//> using lib "com.lihaoyi::ujson:1.4.2"
-//> using lib "com.lihaoyi::os-lib:0.7.5"
+//> using lib "com.softwaremill.sttp.client3::core:3.8.6"
+//> using lib "com.lihaoyi::ujson:2.0.0"
+//> using lib "com.lihaoyi::os-lib:0.9.0"
 //> using option "-deprecation"
 //> using option "-Ywarn-unused"
 
