67f76c8ab5e36e1062524ae43180d6c18090b870
Chris Kipp <ckipp@pm.me>
2023-09-25 14:44:27 +0200

    Update Scala CLI to 1.0.4

--- src/GenerateIndex.scala
+++ src/GenerateIndex.scala
@@ -1,9 +1,8 @@
-//> using scala "3"
-//> using lib "com.softwaremill.sttp.client3::core:3.9.0"
-//> using lib "com.lihaoyi::ujson:3.1.3"
-//> using lib "com.lihaoyi::os-lib:0.9.1"
-//> using option "-deprecation"
-//> using option "-Ywarn-unused"
+//> using scala 3
+//> using dep com.softwaremill.sttp.client3::core:3.9.0
+//> using dep com.lihaoyi::ujson:3.1.3
+//> using dep com.lihaoyi::os-lib:0.9.1
+//> using options -Wunused:all -deprecation
 
 object GenerateIndex {
 
