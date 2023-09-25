3018044ddc6957d012b8504907bb8208a71e9449
Chris Kipp <ckipp@pm.me>
2023-09-25 14:47:10 +0200

    Update sttp to 3.9.0

--- src/GenerateIndex.scala
+++ src/GenerateIndex.scala
@@ -1,5 +1,5 @@
 //> using scala "2.13"
-//> using lib "com.softwaremill.sttp.client3::core:3.8.15"
+//> using lib "com.softwaremill.sttp.client3::core:3.9.0"
 //> using lib "com.lihaoyi::ujson:3.1.3"
 //> using lib "com.lihaoyi::os-lib:0.9.1"
 //> using option "-deprecation"
