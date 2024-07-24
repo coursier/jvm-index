66a57be32a5c75d79413bd743ca104cee08abd8d
Alexandre Archambault <alexandre.archambault@gmail.com>
2024-07-24 14:11:23 +0200

    Updates

--- src/GenerateIndex.scala
+++ src/GenerateIndex.scala
@@ -1,7 +1,7 @@
 //> using scala 3
-//> using dep com.softwaremill.sttp.client3::core:3.9.5
-//> using dep com.lihaoyi::ujson:3.2.0
-//> using dep com.lihaoyi::os-lib:0.9.3
+//> using dep com.softwaremill.sttp.client3::core:3.9.7
+//> using dep com.lihaoyi::ujson:4.0.0
+//> using dep com.lihaoyi::os-lib:0.10.3
 //> using options -Wunused:all -deprecation
 
 object GenerateIndex {
